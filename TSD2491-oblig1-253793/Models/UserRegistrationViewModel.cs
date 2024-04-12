c23f4bd5685175a930a9290c0b72e8b0fbfcc684
253793 <157110562+253793@users.noreply.github.com>
2024-04-12 16:24:38 +0200

    La til UserRegistrationViewModel
    Midlertidig fil lagt til, ikke ferdig implementert.

---
+++ TSD2491-oblig1-253793/Models/UserRegistrationViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TSD2491_oblig1_253793.Models
+{
+    public class UserRegistrationViewModel : Controller
+    {
+        public string Username { get; set; }
+    }
+}
